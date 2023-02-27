f868c4413a59651e9671ee9a16e351d0384a3186
Rega Anton Giapierro <rega22052002@gmail.com>
2023-02-27 16:51:38 +0700

    memperjelas komentar

--- Dicoding/Struktur Data/map.js
+++ Dicoding/Struktur Data/map.js
@@ -1,9 +1,9 @@
 // Map adalah tipe data yang menyimpan koleksi data dengan format key-value layaknya Object. Yang membedakan adalah Map memperbolehkan key dengan tipe data apa pun
 
 const myMap = new Map([
-    ['1', 'a String key'],
-    [1, 'a number key'],
-    [true, true]
+    ['1', 'String'],
+    [1, 'Number'],
+    [true, 'boolean']
 ]);
 
 console.log(myMap);
@@ -14,8 +14,8 @@ const capital = new Map([
     ["Tokyo", "Japan"]
 ]);
 
-console.log(capital.size); // mengetahui jumlah array
-console.log(capital.get("London"));
-capital.set("New Delhi", "India"); // menambahkan data
+console.log(capital.size); // mengetahui jumlah map
+console.log(capital.get("London")); // mengakses nilai
+capital.set("New Delhi", "India"); // fungsi set bisa mengubah atau menetapkan nilai
 console.log(capital.size);
 console.log(capital.get("New Delhi"));
