79d56594b174ae539028be2305c6d7593d34ce29
RegaAG <rega22052002@gmail.com>
2023-03-06 21:40:09 +0700

    Import Export

---
+++ Dicoding/Import Export/index.js
@@ -0,0 +1,6 @@
+import { coffeeStock, isCoffeeMachineReady } from './state.js';
+ 
+console.log(coffeeStock);
+console.log(isCoffeeMachineReady);
+
+// gunakan "coffeeStock as coffie" untuk mengubah pangilan variable
